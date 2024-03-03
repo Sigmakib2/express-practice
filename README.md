a29b9012fbe5e439153b33dbec953087c6fabb25
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2024-03-03 16:38:58 +0600

    proper documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # express-practice
+
+This repository contains the Express Js example code.
+
+Add 2 number in express: [Code](https://)
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world at: '+ port)
-})
+// Route to sum two numbers
+app.get('/sum/:num1/:num2', (req, res) => {
+  const num1 = parseFloat(req.params.num1);
+  const num2 = parseFloat(req.params.num2);
+  const sum = num1 + num2;
+  res.send(`The sum of ${num1} and ${num2} is: ${sum}`);
+});
 
 app.listen(port, () => {
-  console.log(`Hey bro! your server is running on ${port} port!`)
-})
+  console.log(`Server is listening at http://localhost:${port}`);
+});
